2faf81c61c352ed0d50c9cf0757b36973c80c5de
Shaik Rehman <shaikrehman78609@gmail.com>
2025-07-18 23:35:07 +0530

    Update otpApi.jsx

--- frontend/src/api/otpApi.jsx
+++ frontend/src/api/otpApi.jsx
@@ -1,10 +1,12 @@
 // src/api/otpApi.js
 import axios from 'axios';
 
+const BASE_URL = 'https://food-backend-xs3y.onrender.com';
+
 export const sendOtp = async (email) => {
-  return await axios.post('http://localhost:5000/api/otp/send', { email });
+  return await axios.post(`${BASE_URL}/api/otp/send`, { email });
 };
 
 export const verifyOtp = async (email, otp) => {
-  return await axios.post('http://localhost:5000/api/otp/verify', { email, otp });
+  return await axios.post(`${BASE_URL}/api/otp/verify`, { email, otp });
 };
